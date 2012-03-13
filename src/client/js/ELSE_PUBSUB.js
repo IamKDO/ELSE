13e97de4d0929fbc23e4a0ce7f11412fca29b353
jpKDO <kdo@zpmag.com>
2012-03-13 19:33:44 +0100

    Modification arborescence

---
+++ src/client/js/ELSE_PUBSUB.js
@@ -0,0 +1,118 @@
+/* ============================================================================
+	ELSE_PUBSUB.js is part of ELSE, the Extended Library for Surveys Engine
+	Copyright (C) <2011>  <kdo@zpmag.com> 
+	Based on the work of Morgan Roderick http://roderick.dk
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	============================================================================
+
+	Copyright (c) 2010 Morgan Roderick http://roderick.dk
+
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+*/
+
+var PubSub = {};
+(function(p) {
+    
+	p.version = "1.0.1";
+    
+	var messages = {};
+	var lastUid = -1;
+    
+	var publish = function( message, data, sync ) {
+		// if there are no subscribers to this message, just return here
+		if ( !messages.hasOwnProperty( message ) ) {
+			return false;
+		}
+
+		var deliverMessage = function() {
+			var subscribers = messages[message];
+			var throwException = function(e) {
+				return function() {
+					throw e;
+				};
+			};
+			for ( var i = 0, j = subscribers.length; i < j; i++ ) {
+				try {
+					subscribers[i].func( message, data );
+				} catch( e ) {
+					setTimeout( throwException(e), 0);
+				}
+			}
+		};
+		deliverMessage();
+		return true;
+	};
+
+	/**
+	* PubSub.publish( message[, data] ) -> Boolean
+	* - message (String): The message to publish
+	* - data: The data to pass to subscribers
+	* - sync (Boolean): Forces publication to be syncronous, which is more confusing, but faster
+	* Publishes the the message, passing the data to it's subscribers
+	**/
+	p.publish = function( message, data ){
+		return publish( message, data, false );
+	};
+    
+	/**
+	* PubSub.subscribe( message, func ) -> String
+	* - message (String): The message to subscribe to
+	* - func (Function): The function to call when a new message is published
+	* Subscribes the passed function to the passed message. Every returned token is unique and should be
+	* stored if you need to unsubscribe
+	**/
+	p.subscribe = function( message, func ) {
+		// message is not registered yet
+		if ( !messages.hasOwnProperty( message ) ) {
+			messages[message] = [];
+		}
+        
+		// forcing token as String, to allow for future expansions without breaking usage
+		// and allow for easy use as key names for the 'messages' object
+		var token = (++lastUid).toString();
+		messages[message].push( { token : token, func : func } );
+        
+		// return token for unsubscribing
+		return token;
+	};
+
+	/**
+	* PubSub.unsubscribe( token ) -> String | Boolean
+	* - token (String): The token of the function to unsubscribe
+	* Unsubscribes a specific subscriber from a specific message using the unique token
+	**/
+	p.unsubscribe = function( token ){
+		for ( var m in messages ) {
+			if ( messages.hasOwnProperty( m ) ) {
+				for ( var i = 0, j = messages[m].length; i < j; i++ ) {
+					if ( messages[m][i].token === token ) {
+						messages[m].splice( i, 1 );
+						return token;
+					}
+				}
+			}
+		}
+		return false;
+	};
+}(PubSub));
---
+++ src/client/js/ELSE_STRING.js
@@ -0,0 +1,94 @@
+/* ============================================================================
+	ELSE_STRING.js is part of ELSE, the Extended Library for Surveys Engine
+	Copyright (C) <2011>  <kdo@zpmag.com>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	============================================================================
+*/
+
+String.prototype.left = function(n) { 
+	if (n >= this.length) {
+		return this;
+	}
+	return this.substr(0, n);
+};
+
+
+String.prototype.right = function(n) {
+	var L = this.length;
+	if (n >= L) {
+		return this;
+	}
+	return this.substr(L-n, n);
+};
+
+// Trimming functions prototype -----------------------------------------------
+String.prototype.trim = function() {
+	return this.replace(/^\s+/, "").replace(/\s+$/, "");
+};
+
+String.prototype.ltrim = function() {
+	return this.replace(/^\s+/,"");
+};
+
+String.prototype.rtrim = function() {
+	return this.replace(/\s+$/,"");
+}
+// ----------------------------------------------------------------------------
+
+String.prototype.repeat = function(n) {
+	return new Array( n + 1 ).join( this );
+}
+
+// ELSE wrapping
+_ELSE_.CONCAT = function() {
+	var R = "";
+	// iterate through non-separator arguments
+	for (var i = 0; i < arguments.length; i++) {
+		R += arguments[i];
+	}
+ 	return R;
+}
+
+_ELSE_.LEFT = function(str, len) {
+	return str.left(len);
+};
+
+_ELSE_.LOWER = function(str) {
+	return str.toLowerCase();	
+};
+
+_ELSE_.LTRIM = function(str) {
+	return str.ltrim();	
+};
+
+_ELSE_.REPEAT = function(str, n) {
+	return str.repeat(n);
+};
+
+_ELSE_.RIGHT = function(str, len) {
+	return str.right(len);
+};
+
+_ELSE_.RTRIM = function(str) {
+	return str.rtrim();
+};
+
+_ELSE_.TRIM = function(str) {
+	return str.trim();
+};
+
+_ELSE_.UPPER = function(str) {
+	return str.toUpperCase();
+};
